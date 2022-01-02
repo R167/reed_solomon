1aa88616b16fe650746afe6bdf34094adb1a90f3
Winston Durand <me@winstondurand.com>
2022-01-02 09:49:33 -0800

    Init reed_solomon

---
+++ lib/reed_solomon.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "reed_solomon/version"
+
+module ReedSolomon
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/reed_solomon_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ReedSolomon do
+  it "has a version number" do
+    expect(ReedSolomon::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
